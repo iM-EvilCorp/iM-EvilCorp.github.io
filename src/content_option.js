68e4972f411c69e88db8b5044f9e9033cf444673
IMpusing <iM.EvilCorp@Gmail.com>
2022-04-25 01:58:23 +0700

    Portfolio Giokabaneri

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,18 @@
-const logotext = "JOHN";
+const logotext = "Gio";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Portfolio",
+    description: "I’m Giokabaneri data scientist _ Full stack devloper,currently working in Bandung",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Giokabaneri",
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
+        second: "I'm from Bandung",
         third: "I develop mobile apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: "https://previews.dropbox.com/p/thumb/ABhH0WDNBfuvKJST-R5vU-gpxWGbN6VTNP8aS5HDFmijMC0oiT-WslwRCc2N-7LzPCBYdCi1xE1GpKAVSQsk8WBFveV2NfM5bOfN4W3GgGaVSkGW1W0CWUjUjVVx61-15SRyRkSUjN536WlW_K1LfsV96G8KizVqD3pIflA80hOxskihAs_FspHuYQxqXPhgDdM7ziEwmD-2tF4xhhqYsLixgVDksGrXyl7Gilvcvb02DpcCFmQMEOLwYAY7Wf9df8eAzOObe_X9tmXe9G6-y0VcVV5fnWT-R8--ov1o_otsSZNyCBlabkf6vV-Y3CzlwsbQbNAnDYcQjooXxSjtRdqpZqxCCR8cOGtrGYnt7cbNqvB59_b3OeeHFgzjpGAiTj3Yt2S1hILGn0lBRKfmr-5CBtsX-I6W52cHVO1NbRXMS-HxdsXVlTQPXGJJYYPvqW4gMB4fh_Dl0qn7kuOPzjLRWGVHcikq5JQvhj9D3G_Ye5yhYs9CKEWIT0x-ZWATh951btBvREHyHijXEkBgnEJ_fFkc9ruJX5jCVc6v7-tglwIurpC_i6H8xZXpJUofDa_1G1FV4rFepIWxENQYZGlpbW9M5mXQyGIY7PvsqQ9CBtNwnu7i_W2W_aXVeHlOhU14z41RnlwxVI9nObefJ0aVlQ_NVKgcSr_Rjt6Qy8zztDPinxvOSsfTQtdjoQyRWmk566C0DR51J_dnUdWC1DKD8VU7a0F9EW0Z2zCfeHsrZjmaKW7Ee9J4xWdC-CoNn3rfCc4GLiYJv5GLDI7d-wKr51a4J_S-E7t96pfr6tpbGyhIHRV0yLMpjLt5fLGyd9rXxkm2yoqM9MPSGUNZWpUxDqxivGbYB2Ey5Sxh6xSgb2Un6HSuwE7-8lAL4ZKOAo8yiWwiA9LKeDhkJV-At3wdcaMJFd7ihNz4kc9vORPpwXfH_4SpSnBi_gaf9cNXMb4POoFn1ftJBFyBebvArQXswhbzJ0mL4B3szk4ZNgZ2MA/p.png",
 };
 
 const dataabout = {
@@ -21,40 +21,52 @@ const dataabout = {
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        where: "Aku",
+        date: "2017",
     },
     {
         jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        where: "Suka",
+        date: "2018",
     },
     {
         jobtitle: "Designer of week",
-        where: "ALquds",
+        where: "kamu",
         date: "2019",
     },
 ];
 
 const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
+        name: "Flutter",
         value: 85,
     },
     {
-        name: "Javascript",
+        name: "Dart",
+        value: 85,
+    },
+    {
+        name: "java",
         value: 80,
     },
     {
-        name: "React",
+        name: "Android",
+        value: 80,
+    },
+    {
+        name: "iOS",
+        value: 70,
+    },
+    {
+        name: "Xamarim",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Reactive Programming",
+        value: 60,
+    },
+    {
+        name: "Code Magic",
+        value: 60,
     },
 ];
 
@@ -136,8 +148,8 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
+    YOUR_EMAIL: "iM.EvilCorp@Gmail.com",
+    YOUR_FONE: "(+62)",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
